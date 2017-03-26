e95ae6735a7040f58b24102735641ce687e58773
Sairaj Hemachandran <hsairaj@gmail.com>
2017-03-26 18:48:19 +0530

    all changes updated...organize it

--- src/arithmetic.java
+++ src/arithmetic.java
@@ -1,3 +1,10 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package javaapplication18;
+
 
 import java.util.Arrays;
 
@@ -167,14 +174,14 @@ class arithmetic {
              temp1[7]='0';
          }
         switch(c){
-        case 1:   arithmetic.setA(value);
-        case 2:   arithmetic.setB(value);
-        case 3:   arithmetic.setC(value);
-        case 4:   arithmetic.setD(value);
-        case 5:   arithmetic.setE(value);
-        case 6:   arithmetic.setH(value);
-        case 7:   arithmetic.setL(value);
-        case 8:   arithmetic.setL(value);  // INR M how to do?????
+        case 1:   arithmetic.setA(value); break;
+        case 2:   arithmetic.setB(value); break;
+        case 3:   arithmetic.setC(value); break;
+        case 4:   arithmetic.setD(value); break;
+        case 5:   arithmetic.setE(value); break;
+        case 6:   arithmetic.setH(value); break;
+        case 7:   arithmetic.setL(value); break;
+        case 8:   arithmetic.setL(value); break;  // INR M how to do?????
         default: System.out.println("invalid input"); break;
         }
         
@@ -193,14 +200,14 @@ class arithmetic {
              temp1[7]=temp1[7];
          }
         switch(c){
-        case 1:   arithmetic.setA(value);
-        case 2:   arithmetic.setB(value);
-        case 3:   arithmetic.setC(value);
-        case 4:   arithmetic.setD(value);
-        case 5:   arithmetic.setE(value);
-        case 6:   arithmetic.setH(value);
-        case 7:   arithmetic.setL(value);
-        case 8:   arithmetic.setL(value);  // INR M how to do?????
+        case 1:   arithmetic.setA(value); break;
+        case 2:   arithmetic.setB(value); break;
+        case 3:   arithmetic.setC(value); break;
+        case 4:   arithmetic.setD(value); break;
+        case 5:   arithmetic.setE(value); break;
+        case 6:   arithmetic.setH(value); break;
+        case 7:   arithmetic.setL(value); break;
+        case 8:   arithmetic.setL(value); break; // INR M how to do?????
         default: System.out.println("invalid input"); break;
         }
         
@@ -278,12 +285,84 @@ class arithmetic {
             }
         return String.format("%02x",arithmetic.getA());
     }
+////////////////////////////////////////////////////////////////////// //////////////////////////////
+                                //DATA TRANSFER//
+/////////////////////////////////////////////////////////////////////////////////////////////////////
+    //Move immediate instruction                                 //how to differentiate immediate operation ie MVI from MOv
+    public static void MVI(int R1, int R2)
+    {
+        R1 = R2;
+    }
     
     //Move instruction                                 //how to differentiate immediate operation ie MVI from MOv
     public static void MOV(int R1, int R2)
     {
         R1 = R2;
     }
+    
+    //Load Accumulator 
+    public static void LDA(int address){ 
+        int temp = arithmetic.memory[address];
+        arithmetic.setA(temp);
+    }
+    
+    //Store Accumulator content 
+    public static void STA(int address){
+        int temp = arithmetic.getA();
+        arithmetic.memory[address] = temp;
+    }
+    
+    //Load HL register pair
+    public static void LHLD(int address){ 
+        int temp = arithmetic.memory[address];
+        arithmetic.setL(temp);
+        temp = arithmetic.memory[address+1];
+        arithmetic.setH(temp);
+    }
+    
+    //Store HL register pair
+    public static void SHLD(int address){ 
+        int temp = arithmetic.getL();
+        arithmetic.memory[address] = temp;
+        temp = arithmetic.getH();
+        arithmetic.memory[address+1] = temp;
+    }
+    
+    //Load register pair
+    public static void LXI(char X,int word){ 
+        int byte1 = word & 0x00ff;
+        int byte2 = word & 0xff00;
+        switch(X)
+        {
+            case 'H':{
+                        arithmetic.setH(byte2);
+                        arithmetic.setL(byte1);
+                        break;
+                     }
+            case 'B':{
+                        arithmetic.setB(byte2);
+                        arithmetic.setC(byte1);
+                        break;
+                     }
+            case 'D':{
+                        arithmetic.setD(byte2);
+                        arithmetic.setE(byte1);
+                        break;
+                     }
+            default: System.out.println("invalid input");
+        }
+
+    }
+    
+    public static void XCHG(){
+        int temp1 = arithmetic.getH();
+        int temp2 = arithmetic.getL();
+        arithmetic.setH(arithmetic.getD());
+        arithmetic.setL(arithmetic.getE());
+        arithmetic.setD(temp1);
+        arithmetic.setE(temp2);
+    }
+    
     
     
     ///////////////////////////////////////////////////////////////////
